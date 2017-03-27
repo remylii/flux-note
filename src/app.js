20be11bcc18a7bb81c6dfd0c0a2a705a4f273e66
murakami <murakami@ekitan.co.jp>
2017-03-27 13:09:17 +0900

    Update

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    const title = 'Hello, world!';
+    return <h1>{title}</h1>;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-document.getElementById('app').innerHTML = 'Hello, world!';
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './app';
+
+const element = document.getElementById('app');
+
+render(
+  <App />
+  , element
+);
